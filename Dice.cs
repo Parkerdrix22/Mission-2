a2499e35945fdab105e1bf54ee59504a228c8966
Parkerdrix <park2002@byu.edu>
2026-01-16 17:17:53 -0700

    Add project files.

---
+++ Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DiceSimulator
+{
+    internal class Dice
+    {
+        public int[] RollDice(int rolls)
+        {
+            int[] results = new int[13];
+            Random rand = new Random();
+
+            for (int i = 0; i < rolls; i++)
+            {
+                int die1 = rand.Next(1, 7);
+                int die2 = rand.Next(1, 7);
+
+                results[die1 + die2]++;
+            }
+
+            return results;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DiceSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+            int rolls = int.Parse(Console.ReadLine());
+
+            Dice dice = new Dice();
+            int[] results = dice.RollDice(rolls);
+
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + rolls + ".\n");
+
+            for (int i = 2; i <= 12; i++)
+            {
+                int percent = (results[i] * 100) / rolls;
+
+                Console.Write(i + ": ");
+                for (int j = 0; j < percent; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
